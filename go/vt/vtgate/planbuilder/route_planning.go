9e3bcb8aa42944ca71b1ba123d0180aec03886dc
Harshit Gangal <harshit@planetscale.com>
2021-07-15 17:30:17 +0530

    gen4: extracting routing information from predicates for system table query
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/vt/vtgate/planbuilder/route_planning.go
+++ go/vt/vtgate/planbuilder/route_planning.go
@@ -483,7 +483,7 @@ func createRoutePlan(table *abstract.QueryTable, solves semantics.TableSet, vsch
 		if err != nil {
 			return nil, err
 		}
-		return &routePlan{
+		rp := &routePlan{
 			routeOpCode: engine.SelectDBA,
 			solved:      solves,
 			// TODO: find keyspace to route using the predicates as in v3
@@ -496,7 +496,13 @@ func createRoutePlan(table *abstract.QueryTable, solves semantics.TableSet, vsch
 				},
 			}},
 			predicates: table.Predicates,
-		}, nil
+		}
+		err = rp.findSysInfoRoutingPredicatesGen4()
+		if err != nil {
+			return nil, err
+		}
+
+		return rp, nil
 	}
 	vschemaTable, _, _, _, _, err := vschema.FindTableOrVindex(table.Table)
 	if err != nil {
