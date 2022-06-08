d2c918c1c6237218f2a309d9c6a5f5dd6c25cf1b
Dirkjan Bussink <d.bussink@gmail.com>
2022-06-08 14:58:36 +0200

    Add support for alter table rename column (#10469) (#748)
    Right now the parser doesn't support the added syntax in MySQL 8 to
    rename a column directly. This adds support for that.
    
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

--- go/vt/sqlparser/ast_clone.go
+++ go/vt/sqlparser/ast_clone.go
@@ -333,6 +333,8 @@ func CloneSQLNode(in SQLNode) SQLNode {
 		return CloneRefOfRegexpSubstrExpr(in)
 	case *Release:
 		return CloneRefOfRelease(in)
+	case *RenameColumn:
+		return CloneRefOfRenameColumn(in)
 	case *RenameIndex:
 		return CloneRefOfRenameIndex(in)
 	case *RenameTable:
@@ -2075,6 +2077,17 @@ func CloneRefOfRelease(n *Release) *Release {
 	return &out
 }
 
+// CloneRefOfRenameColumn creates a deep clone of the input.
+func CloneRefOfRenameColumn(n *RenameColumn) *RenameColumn {
+	if n == nil {
+		return nil
+	}
+	out := *n
+	out.OldName = CloneRefOfColName(n.OldName)
+	out.NewName = CloneRefOfColName(n.NewName)
+	return &out
+}
+
 // CloneRefOfRenameIndex creates a deep clone of the input.
 func CloneRefOfRenameIndex(n *RenameIndex) *RenameIndex {
 	if n == nil {
@@ -2804,6 +2817,8 @@ func CloneAlterOption(in AlterOption) AlterOption {
 		return CloneRefOfModifyColumn(in)
 	case *OrderByOption:
 		return CloneRefOfOrderByOption(in)
+	case *RenameColumn:
+		return CloneRefOfRenameColumn(in)
 	case *RenameIndex:
 		return CloneRefOfRenameIndex(in)
 	case *RenameTableName:
